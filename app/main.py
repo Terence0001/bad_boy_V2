1e5c2e3ab1f0ccc1801df58efdcc6c69baa96d54
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-12-05 09:41:43 +0400

    Création app streamlit + ajout d'un uploader

---
+++ app/main.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.write("""
+    # Bad Boy Records V2
+""")
+
+
+uploaded_file = st.file_uploader("Upload an song", type=['wav','mp3'])
+if uploaded_file is not None:
+    st.audio(uploaded_file, format="audio/wav")
