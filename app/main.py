d93be70cef806c5ce473f76996e5b86cfa1b37bd
Terence0001 <111972075+Terence0001@users.noreply.github.com>
2023-12-05 15:58:40 +0400

    Installation de PyTorch
    J'ai installé la version qui se base sur les ressources CPU.
    J'ai aussi installé librosa qui nous permettra d'ouvrir les fichiers audio et de les manipuler

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import tempfile
 import streamlit as st
 
 st.write("""
@@ -8,3 +9,6 @@ st.write("""
 uploaded_file = st.file_uploader("Upload an song", type=['wav','mp3'])
 if uploaded_file is not None:
     st.audio(uploaded_file, format="audio/wav")
+
+    tfile = tempfile.NamedTemporaryFile(delete=False)
+    tfile.write(uploaded_file.read())
